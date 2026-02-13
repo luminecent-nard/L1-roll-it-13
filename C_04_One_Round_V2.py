0f03e94ec32ac8527cdf6a5ba6cf629f8e23ab06
luminecent-nard <stapletonr4257@masseyhigh.school.nz>
2026-02-13 14:51:01 +1300

    working single rounds

--- C_04_One_Round_V2.py
+++ C_04_One_Round_V2.py
@@ -4,6 +4,8 @@ import time
 def initial_points(which_player):
     """roll dice twice and return total / if double points apply"""
 
+
+
     double = "no"
     # Roll the dice for the user and note if they got double
     roll_one = random.randint(1, 6)
@@ -15,6 +17,11 @@ def initial_points(which_player):
     print(f"{which_player} \t-Roll 1: {roll_one} \t| Roll 2: {roll_two} \t| Total: {total}")
     return total, double
 
+def make_statement(statement, decoration):
+    """adds emoji/decoration to headings"""
+
+    ends = decoration * 3
+    print(f"\n{ends} {statement} {ends}")
 #main starts here
 
 #roll the dice for the user
@@ -32,7 +39,7 @@ if double_user == "yes":
     print()
     print("you will get double points if you win!")
 #assume user goes first
-first = "user"
+first = "User"
 second = "Bot"
 player_1_points = user_points
 player_2_points = bot_points
@@ -52,30 +59,55 @@ else:
 
 #loop till win
 while player_1_points < 13 and player_2_points < 13:
-
+    print()
     input("press <enter> to continue this round\n")
 
     #first player rolls the dice and score is updated
     print("rolling p1...")
     print()
     time.sleep(0.7)
+    print("rolling p2...")
+    print()
+    time.sleep(0.7)
     player_1_roll = random.randint(1, 6)
     player_1_points += player_1_roll
-    print(f"{first}: rolled a {player_1_roll} - has {player_1_points} points")
+    print(f"{first}:\trolled a {player_1_roll} - has {player_1_points} points")
 
     #if player ones score is over 13 end round
     if player_1_points >= 13:
         break
 
     #player 2 turn and score update
-    time.sleep(0.7)
-    print()
-    print("rolling p2...")
-    print()
-    time.sleep(0.7)
+
     player_2_roll = random.randint(1, 6)
     player_2_points += player_2_roll
 
-    print(f"{second}: rolled a {player_2_roll} - has {player_2_points} points")
+    print(f"{second}:\trolled a {player_2_roll} - has {player_2_points} points")
 
-print("end of round")
+#end of round
+
+# associate player points with either the user r the computer
+user_points = player_1_points
+bot_points = player_2_points
+
+#switch user and bot if they are swapped
+if first == "Bot":
+    user_points, bot_points = bot_points, user_points
+
+#who won?
+if user_points > bot_points:
+    winner = "User"
+else:
+    winner = "Bot"
+round_feedback = f"{winner} wins!!"
+
+#double points if apropriate
+if winner == "user" and double_user == "yes":
+    user_points = user_points * 2
+
+#output results
+make_statement("Round results", "=")
+print(f"user points: {user_points} | Bot points {bot_points}")
+time.sleep(0.7)
+print(round_feedback)
+print()
